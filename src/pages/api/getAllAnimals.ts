54318fe2fef45c822c470a49848baa5baf475e88
erosMariano <erosmariano1@gmail.com>
2023-05-22 19:47:24 -0300

    fix: getAllAnimals

--- src/pages/api/getAllAnimals.ts
+++ src/pages/api/getAllAnimals.ts
@@ -7,13 +7,9 @@ export default async function handleUpload(req: any, res: any) {
 
       res.status(200).json({ animals: animals });
     } catch (error) {
-      console.error("Erro ao obter as URLs das imagens:", error);
       res
         .status(500)
         .json({ error: "Ocorreu um erro ao obter as URLs das imagens" });
     }
   }
-
-
-  return res.status(405).json({ message: "Method Not Allowed" });
 }
